eaff581ff9e44cec378be4d38d13f6d3a97fd6e5
almahfuz <almahfuz0327@gmail.com>
2022-03-14 22:34:00 +0600

    Initial Commit

---
+++ CodeforcesProblems/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CodeforcesProblems
+//
+//  Created by Abdullah Al Mahfuz on 14/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

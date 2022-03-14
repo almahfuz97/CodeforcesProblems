7df8efe737626dc69c61452c6398d86e6b5a836b
almahfuz <almahfuz0327@gmail.com>
2022-03-14 22:45:41 +0600

    problem 4A

---
+++ CodeforcesProblems/4A.cpp
@@ -0,0 +1,25 @@
+//
+//  4A.cpp
+//  CodeforcesProblems
+//
+//  Created by Abdullah Al Mahfuz on 14/3/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int n;
+    cin>>n;
+    
+    if (n%2==0 && n!=2) {
+        cout<<"YES";
+        
+    } else cout<<"NO";
+    
+    return 0;
+}
+
